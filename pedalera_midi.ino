b051d17cc2cd5c51b8ba94b819787d50d0f3850f
nzfs <fonseca.nazareno@gmail.com>
2022-05-31 19:04:12 -0300

    update

--- pedalera_midi.ino
+++ pedalera_midi.ino
@@ -1,9 +1,9 @@
-//----------------//
-// v 0.2          //
-// nzfs 2020      //
-// nzfs@nzfs.net  //
-// nzfs.net       //
-//----------------//
+//---------------|
+// v 0.3         |
+// nzfs 2022     |
+// nzfs@nzfs.net |
+// nzfs.net      |
+//---------------|
 
 #include <MIDI.h>
 #include "Contador.h"
@@ -52,6 +52,10 @@ int CC = 100;
 
 // modos
 String modo;
+String modoPrevio;
+
+// contador inicial
+Contador contadorInicio;
 
 // contador para cambio de modos y ESCENA
 Contador contadorToggle;
@@ -65,7 +69,8 @@ Contador contadorCanalOff;
 
 void setup()
 {
-  Serial.begin(31250);
+  // Serial.begin(31250);
+  Serial.begin(9600);
   MIDI.begin(MIDI_CHANNEL_OMNI);
 
   pinMode(PULSADOR_01, INPUT);
@@ -91,6 +96,10 @@ void setup()
     previo[i] = LOW;
   }
 
+  contadorInicio.contador();
+  contadorInicio.timer = false;
+  
+  // default
   modo = "toggle";
   digitalWrite(RGB_G, HIGH);
   escenaActual = 0;
@@ -106,262 +115,262 @@ void loop()
 {
   leerPulsadores(); // leo el estado de los pulsadores
 
-  // MOMENTARY //
-  if (modo.equals("momentary"))
+  if(contadorInicio.tiempoActual < 3000)
   {
-    for (int i = 0; i < 6; ++i)
-    {
-      if (estadoPulsadores[i] == HIGH)
-      {
-        if (!flag[i])
-        {
-          flag[i] = true;
-          digitalWrite(LED_PULSADOR[i], HIGH);
-          delay(10);
-          MIDI.sendControlChange(CC + i, 127, canalActual + 1);
-        }
-      } else
-      {
-        if (flag[i])
-        {
-          digitalWrite(LED_PULSADOR[i], LOW);
-          delay(10);
-          // MIDI.sendControlChange(CC + i, 0, canalActual + 1);
-          flag[i] = false;
-        }
-      }
-    }
-  } else if (modo.equals("toggle"))
+    // contadorInicio.timer = true;
+    contadorInicio.contador();
+    // Serial.print("inicio");
+  }
+  else
   {
-    // TOGGLE //
-    for (int i = 0; i < 6; ++i)
+    // Serial.print("empezo");
+    
+    // MOMENTARY //
+    if (modo.equals("momentary"))
     {
-      if (estadoPulsadores[i] == HIGH && previo[i] == LOW)
+      for (int i = 0; i < 6; ++i)
       {
-        if (!flag[i])
+        if (estadoPulsadores[i] == HIGH)
         {
-          if (estado[i] == HIGH)
+          if (!flag[i])
           {
+            flag[i] = true;
             digitalWrite(LED_PULSADOR[i], HIGH);
+            delay(10);
             MIDI.sendControlChange(CC + i, 127, canalActual + 1);
-            estado[i] = LOW;
-          } else
+          }
+        } else
+        {
+          if (flag[i])
           {
             digitalWrite(LED_PULSADOR[i], LOW);
-            MIDI.sendControlChange(CC + i, 0, canalActual + 1);
-            estado[i] = HIGH;
+            delay(10);
+            // MIDI.sendControlChange(CC + i, 0, canalActual + 1);
+            flag[i] = false;
+          }
+        }
+      }
+    } else if (modo.equals("toggle"))
+    {
+      // TOGGLE //
+      for (int i = 0; i < 6; ++i)
+      {
+        if (estadoPulsadores[i] == HIGH && previo[i] == LOW)
+        {
+          if (!flag[i])
+          {
+            if (estado[i] == HIGH)
+            {
+              digitalWrite(LED_PULSADOR[i], HIGH);
+              MIDI.sendControlChange(CC + i, 127, canalActual + 1);
+              estado[i] = LOW;
+            } else
+            {
+              digitalWrite(LED_PULSADOR[i], LOW);
+              MIDI.sendControlChange(CC + i, 0, canalActual + 1);
+              estado[i] = HIGH;
+            }
+            delay(10); // debounce
+            flag[i] = true;
+          }
+        } else if (estadoPulsadores[i] == LOW)
+        {
+          if (flag[i])
+          {
+            flag[i] = false;
           }
           delay(10); // debounce
-          flag[i] = true;
         }
-      } else if (estadoPulsadores[i] == LOW)
+        previo[i] = estadoPulsadores[i];
+      }
+    } else if (modo.equals("escena"))
+    {
+      //  SELECTOR DE ESCENAS //
+      for (int i = 0; i < 5; ++i)
       {
-        if (flag[i])
+        if (estadoPulsadores[i] == HIGH)
         {
-          flag[i] = false;
+          escenaActual = i;
         }
-        delay(10); // debounce
-      }
-      previo[i] = estadoPulsadores[i];
-    }
-  } else if (modo.equals("escena"))
-  {
-    //  SELECTOR DE ESCENAS //
-    for (int i = 0; i < 5; ++i)
-    {
-      if (estadoPulsadores[i] == HIGH)
-      {
-        escenaActual = i;
-      }
-      if (escenaActual == i)
-      {
-        digitalWrite(LED_PULSADOR[i], HIGH);
-      } else
-      {
-        digitalWrite(LED_PULSADOR[i], LOW);
-      }
-    }
-    selectorDeEscena(escenaActual);
-
-  } else if (modo.equals("canal"))
-  {
-    if (estadoPulsadores[1] == HIGH)
-    {
-      if (!flagCanalSiguiente && canalActual <= 15)
-      {
-        canalActual++;
-        flagCanalSiguiente = true;
-        delay(10);
-      }
-    } else if (estadoPulsadores[1] == LOW)
-    {
-      flagCanalSiguiente = false;
-    }
-    if (estadoPulsadores[4] == HIGH)
-    {
-      if (!flagCanalAnterior && canalActual >= 0)
-      {
-        canalActual--;
-        flagCanalAnterior = true;
-        delay(10);
-      }
-    } else if (estadoPulsadores[4] == LOW)
-    {
-      flagCanalAnterior = false;
-    }
-    selectorDeCanal(canalActual);
-  }
-
-  // CAMBIO DE MODO //
-  // mantener presionado el pulsador 0 para pasar a modo momentary
-  if (estadoPulsadores[0] == HIGH && modo.equals("toggle") ||
-      estadoPulsadores[0] == HIGH && modo.equals("escena"))
-  {
-    contadorToggle.contador();
-    contadorToggle.timer = false;
-    if (contadorToggle.tiempoActual >= 1000)
-    {
-      if (modo.equals("toggle"))
-      {
-        modo = "momentary";
-        digitalWrite(RGB_R, LOW);
-        digitalWrite(RGB_G, LOW);
-        digitalWrite(RGB_B, HIGH);
-        // apago led
-        apagarLeds();
-        delay(10);
-      }
-    }
-  }
-  else if (estadoPulsadores[0] == LOW)
-  {
-    contadorToggle.timer = true;
-  }
-
-  // mantener presionado el pulsador 1 para pasar el modo toggle
-  if (estadoPulsadores[1] == HIGH && modo.equals("momentary") ||
-      estadoPulsadores[1] == HIGH && modo.equals("escena"))
-  {
-    contadorMomentary.contador();
-    contadorMomentary.timer = false;
-    if (contadorMomentary.tiempoActual >= 1000)
-    {
-      if (modo.equals("momentary"))
-      {
-        modo = "toggle";
-        digitalWrite(RGB_R, LOW);
-        digitalWrite(RGB_G, HIGH);
-        digitalWrite(RGB_B, LOW);
-        delay(10);
-      }
-    }
-  }
-  else if (estadoPulsadores[1] == LOW)
-  {
-    contadorMomentary.timer = true;
-  }
-
-  // mantener presionado el pulsador 5 para el modo escena
-  if (estadoPulsadores[5] == HIGH && modo.equals("toggle") ||
-      estadoPulsadores[5] == HIGH && modo.equals("momentary"))
-  {
-    contadorEscena.contador();
-    contadorEscena.timer = false;
-    if (contadorEscena.tiempoActual >= 1000)
-    {
-      if (modo.equals("momentary") || modo.equals("toggle"))
-      {
-        modo = "escena";
-        digitalWrite(RGB_R, HIGH);
-        digitalWrite(RGB_G, LOW);
-        digitalWrite(RGB_B, LOW);
-        apagarLeds();
-        flagEscena = true;
-        delay(10);
-      }
-    }
-  }
-  else if (estadoPulsadores[5] == LOW)
-  {
-    contadorEscena.timer = true;
-    flagEscena = false;
-  }
-
-  // salir del modo escena
-  if (estadoPulsadores[5] == HIGH && modo.equals("escena") && !flagEscena)
-  {
-    contadorEscenaOff.contador();
-    contadorEscenaOff.timer = false;
-    if (contadorEscenaOff.tiempoActual >= 1000)
-    {
-      if (modo.equals("escena"))
-      {
-        modo = "toggle";
-        digitalWrite(RGB_R, LOW);
-        digitalWrite(RGB_G, HIGH);
-        digitalWrite(RGB_B, LOW);
-        apagarLeds();
-        delay(10);
-      }
-    }
-  }
-  else if (estadoPulsadores[5] == LOW)
-  {
-    contadorEscenaOff.timer = true;
-  }
-
-  // CANAL //
-  // mantener presionado el pulsador 2 para el selector de canal
-  if (estadoPulsadores[2] == HIGH && modo.equals("toggle") ||
-      estadoPulsadores[2] == HIGH && modo.equals("momentary"))
-  {
-    contadorCanal.contador();
-    contadorCanal.timer = false;
-    if (contadorCanal.tiempoActual >= 1000)
-    {
-      if (modo.equals("momentary") || modo.equals("toggle"))
-      {
-        modo = "canal";
-        digitalWrite(RGB_R, HIGH);
-        digitalWrite(RGB_G, HIGH);
-        digitalWrite(RGB_B, HIGH);
-        flagCanal = true;
-        apagarLeds();
-        delay(10);
-      }
-    }
-  }
-  else if (estadoPulsadores[2] == LOW)
-  {
-    contadorCanal.timer = true;
-    flagCanal = false;
-  }
-  // salir del modo canal
-  if (estadoPulsadores[0] == HIGH && modo.equals("canal") && !flagCanal)
-  {
-    contadorCanalOff.contador();
-    contadorCanalOff.timer = false;
-    if (contadorCanalOff.tiempoActual >= 1000)
-    {
-      if (modo.equals("canal"))
-      {
-        for (int i = 0; i < 6; ++i)
+        if (escenaActual == i)
+        {
+          digitalWrite(LED_PULSADOR[i], HIGH);
+        } else
         {
           digitalWrite(LED_PULSADOR[i], LOW);
         }
-        modo = "toggle";
-        digitalWrite(RGB_R, LOW);
-        digitalWrite(RGB_G, HIGH);
-        digitalWrite(RGB_B, LOW);
-        delay(10);
+      }
+      selectorDeEscena(escenaActual);
+  
+    } else if (modo.equals("canal"))
+    {
+      if (estadoPulsadores[1] == HIGH)
+      {
+        if (!flagCanalSiguiente && canalActual <= 15)
+        {
+          canalActual++;
+          flagCanalSiguiente = true;
+          delay(10);
+        }
+      } else if (estadoPulsadores[1] == LOW)
+      {
+        flagCanalSiguiente = false;
+      }
+      if (estadoPulsadores[4] == HIGH)
+      {
+        if (!flagCanalAnterior && canalActual >= 0)
+        {
+          canalActual--;
+          flagCanalAnterior = true;
+          delay(10);
+        }
+      } else if (estadoPulsadores[4] == LOW)
+      {
+        flagCanalAnterior = false;
+      }
+      selectorDeCanal(canalActual);
+    }
+  
+    // CAMBIO DE MODO //
+    // mantener presionado el pulsador 0 para pasar a modo momentary
+    if (estadoPulsadores[0] == HIGH && modo.equals("toggle") ||
+        estadoPulsadores[0] == HIGH && modo.equals("escena"))
+    {
+      contadorToggle.contador();
+      contadorToggle.timer = false;
+      if (contadorToggle.tiempoActual >= 1000)
+      {
+        if (modo.equals("toggle"))
+        {
+          modo = "momentary";
+          ledEstado(modo);
+          // apago led
+          apagarLeds();
+          delay(10);
+        }
       }
     }
+    else if (estadoPulsadores[0] == LOW)
+    {
+      contadorToggle.timer = true;
+    }
+  
+    // mantener presionado el pulsador 1 para pasar el modo toggle
+    if (estadoPulsadores[1] == HIGH && modo.equals("momentary") ||
+        estadoPulsadores[1] == HIGH && modo.equals("escena"))
+    {
+      contadorMomentary.contador();
+      contadorMomentary.timer = false;
+      if (contadorMomentary.tiempoActual >= 1000)
+      {
+        if (modo.equals("momentary"))
+        {
+          modo = "toggle";
+          ledEstado(modo);
+          delay(10);
+        }
+      }
+    }
+    else if (estadoPulsadores[1] == LOW)
+    {
+      contadorMomentary.timer = true;
+    }
+  
+    // mantener presionado el pulsador 5 para el modo escena
+    if (estadoPulsadores[5] == HIGH && modo.equals("toggle") ||
+        estadoPulsadores[5] == HIGH && modo.equals("momentary"))
+    {
+      contadorEscena.contador();
+      contadorEscena.timer = false;
+      if (contadorEscena.tiempoActual >= 1000)
+      {
+        if (modo.equals("momentary") || modo.equals("toggle"))
+        {
+          modoPrevio = modo;
+          modo = "escena";
+          ledEstado(modo);
+          apagarLeds();
+          flagEscena = true;
+          delay(10);
+        }
+      }
+    }
+    else if (estadoPulsadores[5] == LOW)
+    {
+      contadorEscena.timer = true;
+      flagEscena = false;
+    }
+  
+    // salir del modo escena
+    if (estadoPulsadores[5] == HIGH && modo.equals("escena") && !flagEscena)
+    {
+      contadorEscenaOff.contador();
+      contadorEscenaOff.timer = false;
+      if (contadorEscenaOff.tiempoActual >= 1000)
+      {
+        if (modo.equals("escena"))
+        {
+          modo = modoPrevio;
+          ledEstado(modo);
+          apagarLeds();
+          delay(10);
+        }
+      }
+    }
+    else if (estadoPulsadores[5] == LOW)
+    {
+      contadorEscenaOff.timer = true;
+    }
+  
+    // CANAL //
+    // mantener presionado el pulsador 2 para el selector de canal
+    if (estadoPulsadores[2] == HIGH && modo.equals("toggle") ||
+        estadoPulsadores[2] == HIGH && modo.equals("momentary"))
+    {
+      contadorCanal.contador();
+      contadorCanal.timer = false;
+      if (contadorCanal.tiempoActual >= 1000)
+      {
+        if (modo.equals("momentary") || modo.equals("toggle"))
+        {
+          modoPrevio = modo;
+          modo = "canal";
+          ledEstado(modo);
+          flagCanal = true;
+          apagarLeds();
+          delay(10);
+        }
+      }
+    }
+    else if (estadoPulsadores[2] == LOW)
+    {
+      contadorCanal.timer = true;
+      flagCanal = false;
+    }
+    // salir del modo canal
+    if (estadoPulsadores[0] == HIGH && modo.equals("canal") && !flagCanal)
+    {
+      contadorCanalOff.contador();
+      contadorCanalOff.timer = false;
+      if (contadorCanalOff.tiempoActual >= 1000)
+      {
+        if (modo.equals("canal"))
+        {
+          for (int i = 0; i < 6; ++i)
+          {
+            digitalWrite(LED_PULSADOR[i], LOW);
+          }
+          modo = modoPrevio;
+          ledEstado(modo);
+          delay(10);
+        }
+      }
+    }
+    else if (estadoPulsadores[0] == LOW)
+    {
+      contadorCanalOff.timer = true;
+    }
   }
-  else if (estadoPulsadores[0] == LOW)
-  {
-    contadorCanalOff.timer = true;
-  }
-
 }
 
 //-------------------------------------------------------
@@ -403,6 +412,33 @@ void selectorDeEscena(int _escenaActual)
       break;
     case 4:
       break;
+  }
+}
+
+//-------------------------------------------------------
+
+void ledEstado(String _modo)
+{
+  if (_modo.equals("toggle"))
+  {
+    digitalWrite(RGB_R, LOW);
+    digitalWrite(RGB_G, HIGH);
+    digitalWrite(RGB_B, LOW);
+  } else if (_modo.equals("momentary"))
+  {
+    digitalWrite(RGB_R, LOW);
+    digitalWrite(RGB_G, LOW);
+    digitalWrite(RGB_B, HIGH);
+  } else if (_modo.equals("escena"))
+  {
+    digitalWrite(RGB_R, HIGH);
+    digitalWrite(RGB_G, LOW);
+    digitalWrite(RGB_B, LOW);
+  } else if (_modo.equals("canal"))
+  {
+    digitalWrite(RGB_R, HIGH);
+    digitalWrite(RGB_G, HIGH);
+    digitalWrite(RGB_B, HIGH);
   }
 }
 
@@ -589,4 +625,11 @@ void selectorDeCanal(int _canalActual)
       }
       break;
   }
+}
+
+//-------------------------------------------------------
+
+void recibirEstadoToggle()
+{
+  
 }
